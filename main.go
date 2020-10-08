4413abb139ed801287b51e03c20918d24165bff7
Amo Timothy Kofi <timdereaper1@gmail.com>
2020-10-08 01:57:05 +0000

    feat: added contact page

--- main.go
+++ main.go
@@ -7,7 +7,11 @@ import (
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "To get in touch, send an email to <a href=\"mailto:timdereaper1@gmail.com\">timdereaper1@gmail.com</a>.")
+	}
 }
 
 func main() {
